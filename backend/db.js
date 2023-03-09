6b1a15b07e1504545e6168a9c77352b8ad4c05ff
Anonymous <adityakrjha112@gmail.com>
2023-03-09 11:05:58 +0530

    update

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb+srv://adityakrjha112:18K1B6TxOo9ORI6v@inotebook.ch6nv4a.mongodb.net/inotebook?retryWrites=true&w=majority"
+const mongoURI = "input user url"
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
